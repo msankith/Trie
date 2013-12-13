07e179760086e8f96e029b08e906227e1f7565a4
msankith <msankith@gmail.com>
2013-12-13 18:40:10 +0530

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,15 @@
+#include "Node.h"
+
+Node* Node::findChild(char c)
+{
+    for ( int i = 0; i < mChildren.size(); i++ )
+    {
+        Node* tmp = mChildren.at(i);
+        if ( tmp->content() == c )
+        {
+            return tmp;
+        }
+    }
+
+    return NULL;
+}
