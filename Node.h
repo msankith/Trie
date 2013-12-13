e73b81493198c46ee06e1616e6043d272f09bd5f
msankith <msankith@gmail.com>
2013-12-13 18:39:37 +0530

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include<vector>
+using namespace std;
+class Node {
+public:
+    Node() { mContent = ' '; mMarker = false; }
+    ~Node() {}
+    char content() { return mContent; }
+    void setContent(char c) { mContent = c; }
+    bool wordMarker() { return mMarker; }
+    void setWordMarker() { mMarker = true; }
+    Node* findChild(char c);
+    void appendChild(Node* child) { mChildren.push_back(child); }
+    vector<Node*> children() { return mChildren; }
+
+private:
+    char mContent;
+    bool mMarker;
+    vector<Node*> mChildren;
+};
